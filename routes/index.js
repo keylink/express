b8bc25b838767d3effc040892d7cc55dbfc5dd46
Dzhumaliev <kanat.dzhumaliev@sibers.com>
2017-11-21 13:11:37 +0600

    fixed router.redirect to profile

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.post('/register', function(req, res) {
     }
 
     passport.authenticate('local')(req, res, function () {
-      res.redirect('profile/myProfile');
+      res.redirect('/myProfile');
     });
   });
 });
